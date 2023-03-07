bfa250bd43d7b57813cc88d0d3e5b523f6dac6fd
josiah-d <duhaimejosiah@gmail.com>
2023-03-07 10:01:51 -0600

    mv .py, update readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # heic-converter
 Converts HEIC to JPEG (or other)
+
+## Install
+
+`python3 -m pip install pillow-heif`
+
+## Run
+
+`python3 heic_converter.py -p path_to_file -f optional_format`
---
+++ heic_converter.py
@@ -0,0 +1,38 @@
+# pip install pyheif
+import argparse
+
+from PIL import Image
+from pillow_heif import register_heif_opener
+
+register_heif_opener()
+
+
+def open_n_save(path, format="JPEG"):
+    path_not_suffix = path.split(".")[0]
+
+    image = Image.open(path)
+    image.save(f"{path_not_suffix}.{format}", format)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-p",
+        "--path",
+        dest="path",
+        help="path to the .heic image",
+    )
+    parser.add_argument(
+        "-f",
+        "--format",
+        dest="format",
+        help="image format to convert to",
+    )
+
+    args = parser.parse_args()
+
+    if args.format:
+        open_n_save(args.path, args.format.upper())
+    else:
+        open_n_save(args.path)
